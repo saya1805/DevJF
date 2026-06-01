81f54afa9b37581609a752ed95460779885fe306
saya1805 <sayalishelke000@gmail.com>
2026-06-01 15:45:42 +0530

    add changes and popup in application

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 import { CoursesModule } from '../courses/courses.module';
 import { CommonModule } from '@angular/common';
+// import { Router } from 'express';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,10 +12,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+public router = inject(Router)
+
 public Plans = signal(false)
 public selectedOption:any = signal('dashboard')
 public openSettings = signal(false)
 public openCourses = signal(false)
+public openlogout = signal(false)
 
 private Roles = localStorage.getItem('roles')
 
@@ -43,5 +47,19 @@ adminboss = false
       this.adminboss = false
     }
   }
+
+  clicklogout(){
+    this.router.navigate(['/action/logIn'])
+  }
+
+  clickcancel(){
+    this.openlogout.set(false)
+  }
+
+  existpopup(){
+    this.openlogout.set(true)
+  }
+
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined }
+};
+(window as any).util = {
+  inherits: function(ctor: any, superCtor: any) {
+    if (superCtor) {
+      ctor.super_ = superCtor;
+      ctor.prototype = Object.create(superCtor.prototype, {
+        constructor: {
+          value: ctor,
+          enumerable: false,
+          writable: true,
+          configurable: true
+        }
+      });
+    }
+  }
+};
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
